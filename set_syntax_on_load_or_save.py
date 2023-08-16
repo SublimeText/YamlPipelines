fad273cc9c4df352189c73652e39bcc44ba55025
Jason Heeris <jasonheeris@transdigital.com.au>
2023-08-16 12:31:32 +0800

    Expand path pattern based pipeline syntax detection to include *.gitlab-ci.yml and Gitlab composite actions.

--- set_syntax_on_load_or_save.py
+++ set_syntax_on_load_or_save.py
@@ -2,6 +2,16 @@ import sublime
 import sublime_plugin
 from pathlib import Path
 
+# Some CI pipeline scopes can be deduced from their path when given more context
+# than just an extension. This maps a given path pattern to a scope to apply.
+# The first one that matches will be applied (note that dicts are
+# insertion-ordered as of Python 3.7), but in general they should not overlap.
+SCOPE_PATH_PATTERNS = {
+    '**/.github/workflows/**': 'scope:source.yaml.pipeline.github-actions',
+    '**/.github/actions/**/*': 'scope:source.yaml.pipeline.github-actions',
+    '*.gitlab-ci.yml': 'scope:source.yaml.pipeline.gitlab',
+}
+
 
 class YamlFileOpenedEventListener(sublime_plugin.ViewEventListener):
     @classmethod
@@ -16,5 +26,7 @@ class YamlFileOpenedEventListener(sublime_plugin.ViewEventListener):
 
     def set_syntax_if_filepath_applicable(self):
         if filename := self.view.file_name():
-            if Path(filename).match('**/.github/workflows/**'):
-                self.view.assign_syntax('scope:source.yaml.pipeline.github-actions')
+            for pattern, scope in SCOPE_PATH_PATTERNS.items():
+                if Path(filename).match(pattern):
+                    self.view.assign_syntax(scope)
+                    break
