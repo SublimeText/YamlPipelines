5dfd5dec228659d02006abd0e6c1b97d1e47da90
Предраг Николић <idmpepe@gmail.com>
2025-11-03 23:24:17 +0100

    Add Helm.sublime-syntax, support .tpl Helm files, and add globmatch dependency

--- set_syntax_on_load_or_save.py
+++ set_syntax_on_load_or_save.py
@@ -1,6 +1,8 @@
-import sublime
 import sublime_plugin
 from pathlib import Path
+from wcmatch.glob import BRACE
+from wcmatch.glob import globmatch
+from wcmatch.glob import GLOBSTAR
 
 # Some CI pipeline scopes can be deduced from their path when given more context
 # than just an extension. This maps a given path pattern to a scope to apply.
@@ -10,17 +12,13 @@ SCOPE_PATH_PATTERNS = {
     '**/.github/workflows/**': 'scope:source.yaml.pipeline.github-actions',
     '**/.github/actions/**/*': 'scope:source.yaml.pipeline.github-actions',
     '*.gitlab-ci.yml': 'scope:source.yaml.pipeline.gitlab',
-    '*/templates/**/*': 'scope:source.yaml.go',
-    '*/templates/*': 'scope:source.yaml.go',
+    '**/templates/**/*.{yml,yaml,tpl}': 'scope:source.helm'
 }
 
+SUPPORTED_FILE_EXTENSIONS = ['.yml', '.yaml', '.tpl']
 
 class YamlFileOpenedEventListener(sublime_plugin.ViewEventListener):
-    @classmethod
-    def is_applicable(cls, settings):
-        return settings.get('syntax', '') == 'Packages/YAML/YAML.sublime-syntax'
-
-    def on_load_async(self):
+    def on_load(self):
         self.set_syntax_if_filepath_applicable()
 
     def on_save_async(self):
@@ -28,7 +26,9 @@ class YamlFileOpenedEventListener(sublime_plugin.ViewEventListener):
 
     def set_syntax_if_filepath_applicable(self):
         if filename := self.view.file_name():
+            if Path(filename).suffix not in SUPPORTED_FILE_EXTENSIONS:
+                return
             for pattern, scope in SCOPE_PATH_PATTERNS.items():
-                if Path(filename).match(pattern):
+                if globmatch(filename, pattern, flags=GLOBSTAR | BRACE):
                     self.view.assign_syntax(scope)
                     break
