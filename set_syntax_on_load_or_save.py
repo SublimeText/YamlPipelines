bf79924622ba409f6583dd9dcf0fac074b1e6d45
Предраг Николић <idmpepe@gmail.com>
2025-11-04 19:00:01 +0100

    Change approach (:

--- set_syntax_on_load_or_save.py
+++ set_syntax_on_load_or_save.py
@@ -1,23 +1,25 @@
 import sublime_plugin
-from pathlib import Path
 from wcmatch.glob import BRACE
 from wcmatch.glob import globmatch
 from wcmatch.glob import GLOBSTAR
 
-# Some CI pipeline scopes can be deduced from their path when given more context
-# than just an extension. This maps a given path pattern to a scope to apply.
-# The first one that matches will be applied (note that dicts are
-# insertion-ordered as of Python 3.7), but in general they should not overlap.
-SCOPE_PATH_PATTERNS = {
-    '**/.github/workflows/**': 'scope:source.yaml.pipeline.github-actions',
-    '**/.github/actions/**/*': 'scope:source.yaml.pipeline.github-actions',
-    '*.gitlab-ci.yml': 'scope:source.yaml.pipeline.gitlab',
-    '**/templates/**/*.{yml,yaml,tpl}': 'scope:source.helm'
-}
-
-SUPPORTED_FILE_EXTENSIONS = ['.yml', '.yaml', '.tpl']
 
 class YamlFileOpenedEventListener(sublime_plugin.ViewEventListener):
+    # Some CI pipeline scopes can be deduced from their path when given more context
+    # than just an extension. This maps a given path pattern to a scope to apply.
+    # The first one that matches will be applied (note that dicts are
+    # insertion-ordered as of Python 3.7), but in general they should not overlap.
+    SCOPE_PATH_PATTERNS = {
+        '**/.github/workflows/**': 'scope:source.yaml.pipeline.github-actions',
+        '**/.github/actions/**/*': 'scope:source.yaml.pipeline.github-actions',
+        '*.gitlab-ci.yml': 'scope:source.yaml.pipeline.gitlab',
+        '**/templates/**/*.{yml,yaml}': 'scope:source.yaml.helm'
+    }
+
+    @classmethod
+    def is_applicable(cls, settings):
+        return settings.get('syntax', '') == 'Packages/YAML/YAML.sublime-syntax'
+
     def on_load(self):
         self.set_syntax_if_filepath_applicable()
 
@@ -26,9 +28,30 @@ class YamlFileOpenedEventListener(sublime_plugin.ViewEventListener):
 
     def set_syntax_if_filepath_applicable(self):
         if filename := self.view.file_name():
-            if Path(filename).suffix not in SUPPORTED_FILE_EXTENSIONS:
-                return
-            for pattern, scope in SCOPE_PATH_PATTERNS.items():
+            for pattern, scope in self.SCOPE_PATH_PATTERNS.items():
+                if globmatch(filename, pattern, flags=GLOBSTAR | BRACE):
+                    self.view.assign_syntax(scope)
+                    break
+
+
+class PlaintextFileOpenedEventListener(sublime_plugin.ViewEventListener):
+    SCOPE_PATH_PATTERNS = {
+        '**/templates/**/*.tpl': 'scope:source.yaml.helm'
+    }
+
+    @classmethod
+    def is_applicable(cls, settings):
+        return settings.get('syntax', '') == 'Packages/Text/Plain text.tmLanguage'
+
+    def on_load(self):
+        self.set_syntax_if_filepath_applicable()
+
+    def on_save_async(self):
+        self.set_syntax_if_filepath_applicable()
+
+    def set_syntax_if_filepath_applicable(self):
+        if filename := self.view.file_name():
+            for pattern, scope in self.SCOPE_PATH_PATTERNS.items():
                 if globmatch(filename, pattern, flags=GLOBSTAR | BRACE):
                     self.view.assign_syntax(scope)
                     break
